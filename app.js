9d7674aa8c8a6808a2915ae02ecbefe1b1d5c0a2
KaniboyinaRajini <knagaiah02@gmail.com>
2026-09-18 11:48:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      count: 0
+    };
+
+    this.incrementCount = this.incrementCount.bind(this);
+  }
+
+  incrementCount() {
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+
+  render() {
+    return (
+      <div style={{ textAlign: 'center', marginTop: '50px' }}>
+        <h1>Counter App</h1>
+
+        <p>Current Count: {this.state.count}</p>
+
+        <button onClick={this.incrementCount}>
+          Increment
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
